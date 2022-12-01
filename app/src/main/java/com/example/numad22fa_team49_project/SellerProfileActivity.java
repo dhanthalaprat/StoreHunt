9628d816e4085d17477b65d03bde007d80e8a844
NavyaReddyBokka <bokka.n@northeastern.edu>
2022-12-01 14:41:57 -0500

    Seller profile edits

--- app/src/main/java/com/example/numad22fa_team49_project/SellerProfileActivity.java
+++ app/src/main/java/com/example/numad22fa_team49_project/SellerProfileActivity.java
@@ -3,6 +3,9 @@ package com.example.numad22fa_team49_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SellerProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class SellerProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_profile);
+
     }
 }
