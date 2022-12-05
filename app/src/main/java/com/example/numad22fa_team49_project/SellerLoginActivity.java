e85a3065a17bc466d39d7fe481c5f551796187e0
ushaganta <ganta.u@northeastern.edu>
2022-12-05 16:03:56 -0500

    seller login modified

--- app/src/main/java/com/example/numad22fa_team49_project/SellerLoginActivity.java
+++ app/src/main/java/com/example/numad22fa_team49_project/SellerLoginActivity.java
@@ -1,16 +1,71 @@
 package com.example.numad22fa_team49_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SellerLoginActivity extends AppCompatActivity {
 
+    EditText loginEmail, loginPassword;
+    Button loginButton;
+    FirebaseAuth mAuth;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_login);
+        
+        loginButton = findViewById(R.id.seller_login_Button);
+        
+        loginEmail = findViewById(R.id.seller_login_email);
+        loginPassword = findViewById(R.id.seller_login_password);
+        mAuth = FirebaseAuth.getInstance();
+        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+        
+        
+    }
 
+    private void login() {
+        String email = loginEmail.getText().toString();
+        String password = loginPassword.getText().toString();
 
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Please enter a valid email",Toast.LENGTH_SHORT).show();
+            loginEmail.requestFocus();
+        } else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Incorrect password",Toast.LENGTH_SHORT).show();
+            loginPassword.requestFocus();
+        } else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(SellerLoginActivity.this,"User logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SellerLoginActivity.this,SellerProfileActivity.class));
+                        finish();
+                    }else{
+                        Toast.makeText(SellerLoginActivity.this,"Login error: "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
